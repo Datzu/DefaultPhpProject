a5802383a87abda2c9c36ce307f4ca538759a2c6
adrian <adrian@adrian-X55VDR>
2016-03-19 16:32:40 +0100

    v0.0.1 - Default

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/normalize.css" type="text/css" rel="stylesheet" />
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        <script type="text/javascript" src="js/jquery-1.11.3.min.js"></script>
+        <script type="text/javascript" src="js/phaser.js"></script>
+        <script type="text/javascript" src="js/phaser.min.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <script type="text/javascript" src="js/main.js"></script>
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
